2874e00fd0563932a27558e9d2c1ed4114a5a24e
ankugup <ankugup@adobe.com>
2016-02-22 23:43:21 +0530

    with mysql 4.1.2.

--- cookbooks/phpapp/metadata.rb
+++ cookbooks/phpapp/metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures phpapp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-depends 'mysql'
 depends 'apache2'
 depends 'php'
+depends 'mysql', '4.1.2'
 
--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -9,11 +9,11 @@
 
 
 include_recipe 'apache2'
-
- apache_site 'default' do
-
-   enable true
-
-   end
-include_recipe 'php'
-
+include_recipe "mysql::client"
+include_recipe "mysql::server"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "apache2::mod_php5"
+apache_site "default" do
+  enable true
+  end
