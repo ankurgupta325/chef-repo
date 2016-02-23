9d05d4e82c85ae2c0c31ca70f61173d7872aca88
ankugup <ankugup@adobe.com>
2016-02-23 22:41:15 +0530

    added dockerapp

---
+++ cookbooks/dockerapp/recipes/default.rb
@@ -0,0 +1,13 @@
+#
+# Cookbook Name:: dockerapp
+# Recipe:: default
+#
+# Copyright 2016, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+
+docker_service 'default' do
+  action [:create, :start]
+  end
