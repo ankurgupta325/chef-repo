dc5b84be1863a32f6e4fe8b2b713a7529b426bcf
Ankugup <ankugup@adobe.com>
2016-03-01 18:34:53 +0000

    OK now added php

--- cookbooks/phpapp/recipes/default.rb
+++ cookbooks/phpapp/recipes/default.rb
@@ -10,6 +10,9 @@
 
 include_recipe 'apache2'
 include_recipe 'selinux::permissive'
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "apache2::mod_php5"
 apache_site "default" do
   enable true
   end
